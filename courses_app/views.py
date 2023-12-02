47143b0d773185cdeadf1c2d98e37f2ebd6b1fa1
erf1an396 <em.karfarma@gmail.com>
2023-12-02 23:07:48 +0330

    better display

--- courses_app/views.py
+++ courses_app/views.py
@@ -7,5 +7,14 @@ def courses_list(request):
     return render(request,"courses_app/courses_list.html", context={'courses_list':courses})
 
 
-def course_detail(request):
-    pass
+def course_detail(request, id):
+    course = Course.objects.get(id = id )
+    course.views += 1
+    if course.situation == True:
+        course.situation = False
+    else:
+        course.situation = True
+        
+    course.save()
+    return render(request, "courses_app/course_detail.html", context={'course' : course })
+    
--- django2/settings.py
+++ django2/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # my apps 
-    'home_app', 'accounts_app', 'courses_app'
+    'home_app',
+    'accounts_app',
+     'courses_app'
+
 ]
 
 MIDDLEWARE = [
@@ -119,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, "assets"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
